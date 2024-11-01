96a30f42eab49016a1756f123a96b67c022690b3
Jiarui Shu <shujr29@berkeley.edu>
2024-11-01 09:02:21 +0000

    revised index

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,44 +1,41 @@
 class MoviesController < ApplicationController
   before_action :force_index_redirect, only: [:index]
 
-
   def search_tmdb
     # Step 1: Check if both title and release_year are empty
     if params[:title].blank? && params[:release_year].blank?
       flash.now[:alert] = "Please enter at least a title or release year to search."
       render :search_tmdb and return
     end
-  
+
     # Step 2: Fetch movies from TMDb API
     @movies = Movie.find_in_tmdb(title: params[:title], language: params[:language])
-  
+
     # Step 3: Filter movies by release_year, if provided
     if params[:release_year].present?
       @movies = @movies.select do |movie|
         movie[:release_date].present? && movie[:release_date].strftime("%Y") == params[:release_year]
       end
     end
-  
+
     # Step 4: Check if no movies matched the search criteria
     if @movies.empty?
       flash.now[:alert] = "No movies matched your search criteria."
     end
-  
+
     render :search_tmdb
   end
-  
-
 
   def add_movie
     movie_params = {
       title: params[:title],
       release_date: params[:release_date],
       description: params[:overview],
-      # Optionally, add other default attributes here, like rating
+      rating: params[:rating] || 'N/A'  # Default to 'N/A' if rating is not provided
     }
-    
+
     movie = Movie.new(movie_params)
-    
+
     if movie.save
       flash[:notice] = "#{movie.title} was successfully added to RottenPotatoes."
     else
@@ -48,43 +45,27 @@ class MoviesController < ApplicationController
     redirect_to search_tmdb_path
   end
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
   def show
-    id = params[:id] # retrieve movie ID from URI route
-    @movie = Movie.find(id) # look up movie by unique ID
-    # will render app/views/movies/show.<extension> by default
+    @movie = Movie.find(params[:id]) # Retrieve movie by unique ID
   end
 
   def index
     @all_ratings = Movie.all_ratings
-    @movies = Movie.with_ratings(ratings_list, sort_by)
+    selected_ratings = ratings_list
+    sort_column = sort_by || 'id'
+
+    # Fetch movies based on selected ratings and sort order
+    @movies = Movie.with_ratings(selected_ratings, sort_column)
     @ratings_to_show_hash = ratings_hash
-    @sort_by = sort_by
-    # remember the correct settings for next time
-    session['ratings'] = ratings_list
-    session['sort_by'] = @sort_by
+    @sort_by = sort_column
+
+    # Store ratings and sorting preferences in the session
+    session[:ratings] = selected_ratings
+    session[:sort_by] = @sort_by
   end
 
   def new
-    # default: render 'new' template
+    # Default render of 'new' template
   end
 
   def create
@@ -94,11 +75,11 @@ class MoviesController < ApplicationController
   end
 
   def edit
-    @movie = Movie.find params[:id]
+    @movie = Movie.find(params[:id])
   end
 
   def update
-    @movie = Movie.find params[:id]
+    @movie = Movie.find(params[:id])
     @movie.update!(movie_params)
     flash[:notice] = "#{@movie.title} was successfully updated."
     redirect_to movie_path(@movie)
@@ -114,22 +95,31 @@ class MoviesController < ApplicationController
   private
 
   def force_index_redirect
-    return unless !params.key?(:ratings) || !params.key?(:sort_by)
-
-    flash.keep
-    url = movies_path(sort_by: sort_by, ratings: ratings_hash)
-    redirect_to url
+    # Redirect to include ratings and sort_by in the URL if they are missing
+    if !params.key?(:ratings) && !session.key?(:ratings)
+      flash.keep
+      url = movies_path(sort_by: sort_by, ratings: ratings_hash)
+      redirect_to url
+    end
   end
 
   def ratings_list
-    params[:ratings]&.keys || session[:ratings] || Movie.all_ratings
+    # Fetch ratings from params, session, or use default ratings
+    (params[:ratings]&.keys || session[:ratings] || Movie.all_ratings).map(&:to_s)
   end
 
   def ratings_hash
-    ratings_list.to_h { |item| [item, "1"] }
+    # Convert ratings list to hash format for URL parameters
+    ratings_list.to_h { |rating| [rating, "1"] }
   end
 
   def sort_by
-    params[:sort_by] || session[:sort_by] || 'id'
+    # Ensure sort_by parameter is a valid column name
+    valid_columns = Movie.column_names
+    params[:sort_by] if valid_columns.include?(params[:sort_by])
+  end
+
+  def movie_params
+    params.require(:movie).permit(:title, :rating, :description, :release_date)
   end
 end
